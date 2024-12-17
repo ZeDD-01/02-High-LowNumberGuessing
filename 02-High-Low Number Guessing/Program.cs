21d57f9e552db83ab37c11f2a50a1f3707239949
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2024-12-18 00:47:56 +0100

    First Version

--- 02-High-Low Number Guessing/Program.cs
+++ 02-High-Low Number Guessing/Program.cs
@@ -6,21 +6,36 @@ class Program
     {
         Random rnd = new Random();
         int randomNumber = rnd.Next(1, 100);
-
-        int[] searchArray = new int[101];
-
-        for (int i = 0; i <= 100; i++)
-        {
-            searchArray[i] = i;
-            
-        }
         
         Console.WriteLine("Hi, let's play a game! Can you guess the number between 1 and 100?");
         int guessedNumber = int.Parse(Console.ReadLine());
+        int tries = 0;
+
+        while (guessedNumber != randomNumber)
+        {
+            if (guessedNumber < randomNumber)
+            {
+                Console.WriteLine("Too low, try again!");
+            }
+            if(guessedNumber > randomNumber)
+            {
+                Console.WriteLine("Too high, try again!");
+            }
+
+            if (tries > 10)
+            {
+                Console.WriteLine("Sorry, you had too many tries");
+                return;
+            }
+
+            if (guessedNumber + 5 == randomNumber || guessedNumber - 5 == randomNumber)
+            {
+                Console.WriteLine("You're close!");
+            }
+            guessedNumber = int.Parse(Console.ReadLine());
+            tries++;
+        }
         
-        
-        
-        Console.WriteLine("The random number is: " + randomNumber);
-        Console.WriteLine(guessedNumber);
+        Console.WriteLine("Correct! The random number is: " + randomNumber);
     }
 }
