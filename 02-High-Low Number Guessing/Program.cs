830a36e01055db7b046e9b6c669461bb3bf06e7e
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2024-12-18 00:08:15 +0100

    Generating new random number and filling the search array

--- 02-High-Low Number Guessing/Program.cs
+++ 02-High-Low Number Guessing/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1, 100);
+
+        int[] searchArray = new int[101];
+
+        for (int i = 0; i <= 100; i++)
+        {
+            searchArray[i] = i;
+            
+        }
+        
+        Console.WriteLine("Hi, let's play a game! Can you guess the number between 1 and 100?");
+        int guessedNumber = int.Parse(Console.ReadLine());
+        
+        
+        
+        Console.WriteLine("The random number is: " + randomNumber);
+        Console.WriteLine(guessedNumber);
     }
 }
