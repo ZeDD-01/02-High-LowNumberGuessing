b8698edf67dfba915d6e54ea806742cad8d49d8e
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2024-12-19 12:37:31 +0100

    Removed magic numbers

--- 02-High-Low Number Guessing/Program.cs
+++ 02-High-Low Number Guessing/Program.cs
@@ -5,12 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Random rnd = new Random();
-        int minRandomNumber = 1;
-        int maxRandomNumber = 100;
-        int randomNumber = rnd.Next(minRandomNumber, maxRandomNumber);
+        const int MIN_RANDOM_NUMBER = 1;
+        const int MAX_RANDOM_NUMBER = 100;
+        int randomNumber = rnd.Next(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
         Console.WriteLine($"Random number: {randomNumber}");
-        
-        
         
         Console.WriteLine("Hi, let's play a game! Can you guess the number between 1 and 100?");
         int guessedNumber = int.Parse(Console.ReadLine());
@@ -27,14 +25,15 @@ class Program
                 Console.WriteLine("Too high, try again!");
             }
 
-            int maxTries = 10;
-            if (tries > maxTries)
+            const int MAX_TRIES = 10;
+            if (tries > MAX_TRIES)
             {
                 Console.WriteLine("Sorry, you had too many tries");
                 return;
             }
 
-            if ((Math.Abs(randomNumber - guessedNumber)) <= 5)
+            const int DISTANCE_TO_GUESSED_NUMBER = 5;
+            if ((Math.Abs(randomNumber - guessedNumber)) <= DISTANCE_TO_GUESSED_NUMBER)
             {
                 Console.WriteLine("But you're close!");
             }
